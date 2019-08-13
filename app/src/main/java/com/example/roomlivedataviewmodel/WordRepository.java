5387b50b2b291328b0781c757a5d2d11e52b9318
tsjackson <tylerj3@gmail.com>
2019-08-13 12:28:00 -0400

    First Commit

---
+++ app/src/main/java/com/example/roomlivedataviewmodel/WordRepository.java
@@ -0,0 +1,47 @@
+package com.example.roomlivedataviewmodel;
+
+import android.app.Application;
+import android.os.AsyncTask;
+
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+
+//Repository implements the logic for deciding
+// whether to fetch data from a network or use
+// results cached in the local database.
+
+public class WordRepository {
+    private  WordDao mWordDao;
+    private LiveData<List<Word>> mAllWords;
+
+    //constructor that gets a handle to the DB
+    // and initializes the member variables
+
+    WordRepository(Application application){
+        WordRoomDatabase db = WordRoomDatabase.getDatabase(application);
+        mWordDao = db.wordDao();
+        mAllWords = mWordDao.getAllWords();
+    }
+
+    // This is called a wrapper, this ones job is to return a cached
+    // words as LiveData.
+    LiveData<List<Word>> getmAllWords(){return mAllWords;}
+
+    public void insert (Word word){
+        new insertAsyncTask(mWordDao).execute(word);
+    }
+
+    private static class insertAsyncTask extends AsyncTask<Word, Void, Void>{
+        private WordDao mAsyncTaskDao;
+
+        insertAsyncTask(WordDao dao){
+            mAsyncTaskDao = dao;
+        }
+        @Override
+        protected Void doInBackground(final Word...params){
+            mAsyncTaskDao.insert(params[0]);
+            return null;
+        }
+    }
+}
